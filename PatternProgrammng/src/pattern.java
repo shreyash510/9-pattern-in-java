6738e61f94f2a59c24d297641148225ec29e6380
shreyeshkolhe510 <shreyashkolhe2001@gmail.com>
2022-05-23 13:38:11 +0530

    1st pattern

---
+++ PatternProgrammng/src/pattern.java
@@ -0,0 +1,19 @@
+public class pattern {
+    /* 1st pattern */
+//            * * * * *
+//            * * * * *
+//            * * * * *
+//            * * * * *
+//            * * * * *
+    public static void main(String arg[]){
+        int n = 5;
+        for(int i= 0; i<n; i++){
+            for(int j =0; j<n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
