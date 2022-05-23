597b06dc432cb644b73903827331e14488fed2cf
shreyeshkolhe510 <shreyashkolhe2001@gmail.com>
2022-05-23 14:09:19 +0530

    2nd pattern

--- PatternProgrammng/src/pattern.java
+++ PatternProgrammng/src/pattern.java
@@ -1,11 +1,17 @@
 public class pattern {
-    /* 1st pattern */
-//            * * * * *
-//            * * * * *
-//            * * * * *
-//            * * * * *
-//            * * * * *
     public static void main(String arg[]){
+        pattern p1 = new pattern();
+//        p1.pattern1();
+        p1.pattern2();
+
+    }
+    public void pattern1(){
+        /* 1st pattern */
+//            * * * * *
+//            * * * * *
+//            * * * * *
+//            * * * * *
+//            * * * * *
         int n = 5;
         for(int i= 0; i<n; i++){
             for(int j =0; j<n; j++){
@@ -15,5 +21,25 @@ public class pattern {
         }
     }
 
-    
+    public void pattern2(){
+        /*2nd pattern*/
+//* * * * *
+//*       *
+//*       *
+//*       *
+//* * * * *
+        int n = 5;
+        for(int i = 0; i<n; i++){
+            for(int j = 0 ; j < n; j++){
+                if(j==0 || j==n-1 ||i==0 || i == n-1){
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
 }
